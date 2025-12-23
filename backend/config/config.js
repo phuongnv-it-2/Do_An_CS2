5c9ddd9936e545c6a992a8aacf206232cd7bafa3
phuongnv-it-2 <nvphuongaz@gmail.com>
2025-12-24 00:12:51 +0700

    Fix Sequelize env config for Render (use process.env)

---
+++ backend/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "",
+    database: "test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
